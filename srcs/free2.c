810d4463fe8e29c79b74beec759ae06e97e2c3c8
R6145 <fmaqdasi@DESKTOP-MDILJUS>
2024-11-13 12:44:17 +0400

    changed the textures and the error messages

--- srcs/free2.c
+++ srcs/free2.c
@@ -6,7 +6,7 @@
 /*   By: fmaqdasi <fmaqdasi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 15:23:22 by fmaqdasi          #+#    #+#             */
-/*   Updated: 2024/11/09 15:23:24 by fmaqdasi         ###   ########.fr       */
+/*   Updated: 2024/11/13 12:42:27 by fmaqdasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,21 @@ void	error(t_cub3d *cube, int flag)
 	if (flag == 0)
 		exit (1);
 	if (flag == 1)
-		ft_putstr_fd("Error: No File Name\n", 2);
+		ft_putstr_fd("No File Name\n", 2);
 	if (flag == 2)
-		ft_putstr_fd("Error: Map wrong\n", 2);
+		ft_putstr_fd("Map wrong\n", 2);
 	if (flag == 3)
-		ft_putstr_fd("Error: Wrong number of players\n", 2);
+		ft_putstr_fd("Wrong number of players\n", 2);
 	if (flag == 4)
-		ft_putstr_fd("Error: Ceiling or Floor, wrong color\n", 2);
+		ft_putstr_fd("Wrong color parameter given\n", 2);
 	if (flag == 5)
-		ft_putstr_fd("Error: Textures doesn't exist\n", 2);
+		ft_putstr_fd("Missing Textures\n", 2);
 	if (flag == 6)
-		ft_putstr_fd("Error: Map has empty line\n", 2);
+		ft_putstr_fd("Map has empty line\n", 2);
 	if (flag == 7)
-		ft_putstr_fd("Error: Map bad\n", 2);
+		ft_putstr_fd("Map is Not Good\n", 2);
 	if (flag == 8)
-		ft_putstr_fd("Error: Empty file\n", 2);
+		ft_putstr_fd("Empty file\n", 2);
 	close_free(cube);
 	exit(1);
 }
